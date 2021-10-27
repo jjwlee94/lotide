b23b56de2c17c66de4c6a31ea7a54dac058fa1b9
Jeewon Lee <jeewon.jlee@gmail.com>
2021-10-27 04:31:42 +0000

    add assertEqual function

---
+++ findKeyByValue.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  let a = actual;
+  let b = expected;
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
+  }
+};
