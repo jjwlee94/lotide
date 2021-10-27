dae13cfb71ebb5e21efe73d1393c151c39e09958
Jeewon Lee <jeewon.jlee@gmail.com>
2021-10-27 13:22:07 +0000

    Add framework

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,27 @@
-const letterPositions = function(sentence) {
-  const results = {};
+ const eqArrays = function(firstArr, secondArr) {
+  for (var i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== secondArr[i]) {
+      return false;
+    }
+  } 
+  return true;
+}
+const assertArraysEqual = function(firstArr, secondArr) {
+  if (eqArrays(firstArr, secondArr)) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArr} === ${secondArr}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArr} !== ${secondArr}`);
+  }
+}
 
-  return results;
+const letterPositions = function(sentence) {
+  const result = {};
+  // Use for loop to loop through sentence[i]
+  // If letter is in result, push index position
+  // If letter is not in result, add to object
+  console.log(result);
+  return result;
 };
+
+assertArraysEqual(letterPositions("hello").e, [1]); // => pass
+assertArraysEqual(letterPositions("hello").h, [1]); // => fail
