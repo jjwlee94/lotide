f5215814efe41e7ee34e41d928e74bf1a5fcfe29
Jeewon Lee <jeewon.jlee@gmail.com>
2021-10-27 03:07:37 +0000

    Define function

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  let a = actual;
+  let b = expected;
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
+  }
+};
+
+const countLetters = function() {
+  
+}
