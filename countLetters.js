ce56e7bfe79f94f9ff4bb15d9d064d5720e188b0
Jeewon Lee <jeewon.jlee@gmail.com>
2021-10-27 03:53:32 +0000

    Working function

--- countLetters.js
+++ countLetters.js
@@ -8,6 +8,22 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countLetters = function() {
-  
+const countLetters = function(string) {
+  const result = {};
+  const allLetters = string.split('');
+  for (const letter of allLetters) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+    console.log(result);
+  }
+  return result;
 }
+
+const string = "LHL";
+const result = countLetters(string);
+
+assertEqual(result["L"], 2);
+assertEqual(result["H"], 1);
