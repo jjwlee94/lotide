46a20d2e6e026c21fbe91af1f3fdfee2eed628b0
Jeewon Lee <jeewon.jlee@gmail.com>
2021-10-28 13:49:02 +0000

    Add assertEqual function

---
+++ findKey.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  let a = actual;
+  let b = expected;
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
+  }
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
