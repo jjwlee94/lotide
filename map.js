62eac459d11eeb5a82e5db223ae1e734e1c14708
Jeewon Lee <jeewon.jlee@gmail.com>
2021-10-28 13:07:28 +0000

    Test code

--- map.js
+++ map.js
@@ -1,9 +1,35 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function() {
+const map = function(array, callback) {
   const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
   return results;
 }
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
+
+const eqArrays = function(firstArr, secondArr) {
+  if (firstArr.length !== secondArr.length) {
+    return false;
+  } else {
+    for (let i = 0; i < firstArr.length; i++) {
+      if (firstArr[i] !== secondArr[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+assertArraysEqual(['g', 'c', 't', 'm', 't'], ['g', 'c', 't', 'm', 't']); // => should PASS
