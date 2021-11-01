69dd860e2f093129603427040bac630ae810dc1d
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-01 04:00:04 +0000

    Refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(firstArr, secondArr) {
-  if (firstArr.length !== secondArr.length) {
-    return false;
-  } else {
-    for (let i = 0; i < firstArr.length; i++) {
-      if (firstArr[i] !== secondArr[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -19,8 +8,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 }
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], [1, "2", "3"]); // => false
-assertArraysEqual(["hello"], ["hello"]); // => true
+module.exports = assertArraysEqual;
